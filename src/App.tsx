9f4f508b98da8d2b705d2b66ed6a1f0d34fd3c38
gyawali9 <roshangyawali9@gmail.com>
2022-12-17 19:00:49 +0545

    typing array prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
   const personName = {
     first: "Bruce",
-    last: "Wayne"
-  }
+    last: "Wayne",
+  };
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: "Wayne",
+    },
+    {
+      first: "Clark",
+      last: "Kent",
+    },
+    {
+      first: "Princess",
+      last: "Diana",
+    },
+  ];
+
   return (
     <div className="App">
       <p>Holle</p>
-      <Greet name="Roshan" messageCount={10} isLoggedIn={false}/>
-      <Person name={personName}/>
+      <Greet name="Roshan" messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
